32445a232f94f30391fde24a07148f4d5358c29f
ryry-shi <ryadboulahrouf84@outlook.fr>
2024-02-07 23:20:47 +0100

    correct purchasePlace

--- server.py
+++ server.py
@@ -54,10 +54,23 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+    if (placesRequired < 0):
+        flash("Vous ne pouvez pas mettre de nombre négative")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    if (int(club["points"]) - placesRequired < 0):
+        flash("Vous n'avez pas assez de points pour réservez ce montant de ticket")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    if placesRequired > 12:
+        flash("Vous ne pouvez pas réservez plus de 12 place")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    if competition['numberOfPlaces'] == 0:
+        flash('Great-booking complete!')
+    if int(competition["numberOfPlaces"]) - placesRequired < 0:
+        flash("Il y a pas assez de places dans la competitions")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    club["points"] = int(club["points"]) - placesRequired
+    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
     return render_template('welcome.html', club=club, competitions=competitions)
-
 
 # TODO: Add route for points display
 
